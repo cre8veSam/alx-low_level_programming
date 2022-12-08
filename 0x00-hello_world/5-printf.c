8297a825b00cebd892d5340aae4b90174369a7bc
cre8veSam <oderinwalesamson1@gmail.com>
2022-12-08 12:47:50 -0800

    using C program to print a string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * Description: print "with proper grammar, but the outcome is a piece of art,"
+ *
+ * return 0 (successful)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
